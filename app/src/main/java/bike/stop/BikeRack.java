7271e3c8f9d843b21a52f19dda28fd6f28d2266b
Duke Nguyen <nguyentranduc1@gmail.com>
2016-03-12 16:35:48 -0800

    Add a class and file

---
+++ app/src/main/java/bike/stop/BikeRack.java
@@ -0,0 +1,16 @@
+package bike.stop;
+import java.util.ArrayList;
+
+public class BikeRack {
+    public final double longtitude;
+    public final double latitude;
+
+    public BikeRack(double longitude, double latitude ){
+        this.longtitude = longitude;
+        this.latitude = latitude;
+    }
+
+//    public static ArrayList<BikeRack> getBikeRacks(){
+//        return ;
+//    }
+}
